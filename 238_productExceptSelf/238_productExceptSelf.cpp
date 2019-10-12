c24e846b493cdfa5032c9f64db1abdf72996caec
change <changyy66@163.com>
2019-10-12 11:57:59 +0800

    add

--- 238_productExceptSelf/238_productExceptSelf.cpp
+++ 238_productExceptSelf/238_productExceptSelf.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+class Solution {
+public:
+	vector<int> productExceptSelf(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> res(n, 1);
+
+		int left = 1, right = 1;
+		for (int i = 0; i < n; i++)
+		{
+			res[i] *= left;
+			left *= nums[i];
+
+			res[n - 1 - i] *= right;
+			right *= nums[n - 1 - i];
+		}
+		return res;
+	}
+};
 
 int main()
 {
+	Solution s;
+	vector<int> v = { 1,2,3,4 };
+	vector<int> res = s.productExceptSelf(v);
+	for each (auto var in res)
+	{
+		cout << var << " ";
+	}
     return 0;
 }
 
